b9e070241b895fa028f7382fd7e7932426ef0983
CoffeeBe4r <L20321106@acapulco.tecnm.mx>
2025-05-16 17:00:20 -0600

    feat: add AmbientPlayer component to App for enhanced audio experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 
 import './App.css'
+import AmbientPlayer from './components/AmbientPlayer/AmbientPlayer';
 import Pomodoro from './components/Pomodoro/Pomodoro';
 import TaskList from './components/TaskList/TaskList';
+
 
 function App() {
   return (
     <div className="App">
-     
       <Pomodoro />
+      <AmbientPlayer  />
       <TaskList />
     </div>
-  );
+    );
 }
 
 export default App;
